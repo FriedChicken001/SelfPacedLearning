737063e6650c8493be3c355cd9c975294ff88dc6
Jessica Major <jessica.g.major@gmail.com>
2024-02-21 22:05:51 -0700

    Challenge: Create a small interactive game where the user needs to math the problems correctly.

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -2,30 +2,38 @@
 {
     class Program
     {
+        public static object Conosle { get; private set; }
+
         static void Main(string[] args)
         {
-            int age;
-            int height;
+            // CHALLENGE: provide the user a problem but create an if/else statement to check the answer
 
-            Console.Write("Please give your age: ");
-            age = Convert.ToInt32(Console.ReadLine());
+            int answer;
 
-            Console.Write("Please provide your height (inches): ");
-            height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please solve the following problem: \nWhat is 5 * 8 / 4 = ");
+             answer = Convert.ToInt32(Console.ReadLine());
 
-            if (age >= 18) 
+            if (answer == 4)
             {
-                if(height >= 60)
+                Console.WriteLine("You are so smart!");
+
+                Console.WriteLine("Try this one out! \nWhat is 10 + 10 / 2 = ");
+                 answer = Convert.ToInt32(Console.ReadLine()); ;
+                if (answer == 10)
                 {
-                    Console.WriteLine("You can enter!"); // having a condition within a condition is called Nesting
+                    Console.WriteLine("Wow~! You are killing it! But can you keep it up?");
+                    Console.WriteLine("Finally, what is 100 / 2  = ");
+                    answer = Convert.ToInt32(Console.ReadLine());
                 }
                 else
                 {
-                    Console.WriteLine("You do not meet the requirements.");
+                    Console.WriteLine("Oof. That was rough. You can't try again but you did something.");
                 }
-
+            } 
+            else
+            {
+                Console.WriteLine("Stop wasting time and find a calculator");
             }
-           
 
             Console.ReadKey();
         }

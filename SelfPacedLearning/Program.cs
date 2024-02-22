36e98de55228dcfb68b5baf9d19cf36ae9932239
Jessica Major <jessica.g.major@gmail.com>
2024-02-22 14:40:16 -0700

    Breakdown of arrays

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -6,40 +6,15 @@
 
         static void Main(string[] args)
         {
-            // CHALLENGE: Roll 2 die and loop it until you get two of a kind
-            Random numberGen = new Random();
+            string[] movies = { "Lord of the Rings", "Fight Club", "Interstellar", "Gladiator" };
 
-            int roll01 = 0;
-            int roll02 = 0;
-            int attempts = 1;
-
-            Console.WriteLine("Press enter to roll the die.");
-
-            while(true)
+            for (int i = 0; i < movies.Length; i++) //instead of using a numerical number, we can just summon the legnth of the entire array
             {
+                int rank = i + 1; // this is tel tell us what rank the movies are "what order they are in the index"
 
-                Console.ReadKey(); // this will make it to where we wait for user input before proceeding
-
-                roll01 = numberGen.Next(1, 7); /// first number is the INCLUSIVE minimum, second number is the EXCLUDED maximum or the limit
-                Console.WriteLine("Die 1: " + roll01);
-                roll02 = numberGen.Next(1, 7);
-                Console.WriteLine("Die 2: " + roll02);
-
-                if (roll01 == roll02 || roll02 == roll01)
-                {
-
-                    Console.WriteLine(" It took you " + attempts + " attempts to roll a double");
-
-                    break;
-                }
-                else
-                {
-                    attempts++;
-                }
-                /// this means, after each attempt, increase attempt + 1
+                Console.WriteLine(rank + " " + movies[i]); 
+            
             }
-            // the while loop whill continue until the condition is met, in this case its hitting up to "6" on the die or the number before 7
-
 
             Console.ReadKey();
         }

0c1bb3883e0d6373847fea40521932b9758ccef8
Jessica Major <jessica.g.major@gmail.com>
2024-02-22 16:03:38 -0700

    Method example'

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -10,29 +10,28 @@ namespace Self_Paced_Learning
 
         static void Main(string[] args)
         {
-            // CHALLENGE: create a program that a teacher can use to input their students and sort them alphabettically 
+            MeetAlien();
 
-            string[] students = new string[4];
-
-            Console.WriteLine("Please provide the studnet's name: ");
-
-            for (int i = 0; i < students.Length; i++)
-            {
-                students[i] = Console.ReadLine();
-            }
-
-            Console.WriteLine("Thank you for your input! \nHere are your students in alphabetical order: ");
-            Array.Sort(students);
-
-            for (int i = 0;i < students.Length; i++)
-            {
-                Console.WriteLine(students[i]);
-            }
-              
-            
             
 
             Console.ReadKey();
         }
+
+        static void MeetAlien()
+        {
+            Random numbergen = new Random();
+
+            string name = "X-" + numbergen.Next(10, 9999);
+
+            int age = numbergen.Next(10, 500);
+
+            Console.WriteLine("Hi, I'm " + name);
+            Console.WriteLine("I'm " + age + " years old.");
+            Console.WriteLine("Oh, and I'm an alien.");
+
+        }
     }
+
+
+
 }

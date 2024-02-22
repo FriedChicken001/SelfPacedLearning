5d74d266ea5b498c091a309983570543a37de58b
Jessica Major <jessica.g.major@gmail.com>
2024-02-21 22:20:57 -0700

    Lesson 4, using math functions in code

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -6,33 +6,11 @@
 
         static void Main(string[] args)
         {
-            // CHALLENGE: provide the user a problem but create an if/else statement to check the answer
-
-            int answer;
-
-            Console.Write("Please solve the following problem: \nWhat is 5 * 8 / 4 = ");
-             answer = Convert.ToInt32(Console.ReadLine());
-
-            if (answer == 4)
+           for (int i = 1;i <= 10;i++) // this will count up to 10
+                //to count down (int i = 10; i > 0; i --)
             {
-                Console.WriteLine("You are so smart!");
-
-                Console.WriteLine("Try this one out! \nWhat is 10 + 10 / 2 = ");
-                 answer = Convert.ToInt32(Console.ReadLine()); ;
-                if (answer == 10)
-                {
-                    Console.WriteLine("Wow~! You are killing it! But can you keep it up?");
-                    Console.WriteLine("Finally, what is 100 / 2  = ");
-                    answer = Convert.ToInt32(Console.ReadLine());
-                }
-                else
-                {
-                    Console.WriteLine("Oof. That was rough. You can't try again but you did something.");
-                }
-            } 
-            else
-            {
-                Console.WriteLine("Stop wasting time and find a calculator");
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
             }
 
             Console.ReadKey();

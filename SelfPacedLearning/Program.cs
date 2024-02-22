4e8da8e0012953f591dfcee43496e1857c6e7303
Jessica Major <jessica.g.major@gmail.com>
2024-02-22 14:50:12 -0700

    Using for loops to grab information from a blank array

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -2,18 +2,26 @@
 {
     class Program
     {
-        public static object Conosle { get; private set; }
 
         static void Main(string[] args)
         {
-            string[] movies = { "Lord of the Rings", "Fight Club", "Interstellar", "Gladiator" };
+            string[] movies = new string[4]; // inside the second string[]. you can input the legnth of the array
+            // ^^ this is an empty array that the user inputs the information
 
-            for (int i = 0; i < movies.Length; i++) //instead of using a numerical number, we can just summon the legnth of the entire array
+            Console.WriteLine("Type in 4 movies");
+
+            for (int i = 0; i < movies.Length; i++)
             {
-                int rank = i + 1; // this is tel tell us what rank the movies are "what order they are in the index"
-
-                Console.WriteLine(rank + " " + movies[i]); 
+                movies[i] = Console.ReadLine(); // <-- this is storing values in the array
+            }
             
+            Console.WriteLine("\nHere they are alphabetically: ");
+
+            Array.Sort(movies); // this will sort the array in order alphabettically (or numerically if its a number)
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(movies[i]);
             }
 
             Console.ReadKey();

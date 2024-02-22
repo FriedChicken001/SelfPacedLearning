af25cf6a6cac66028946617aeddf6cd2b14623ec
Jessica Major <jessica.g.major@gmail.com>
2024-02-21 22:26:34 -0700

    Using user input in order to determine the loop's condition

--- SelfPacedLearning/Program.cs
+++ SelfPacedLearning/Program.cs
@@ -6,10 +6,19 @@
 
         static void Main(string[] args)
         {
-           for (int i = 1;i <= 10;i++) // this will count up to 10
+            // how to allow the user to determine the number of lines we use
+
+            Console.Write("How many cool numbers do you want: ");
+
+            int count = Convert.ToInt32(Console.ReadLine());
+
+           for (int i = 1;i <= count;i++) // will repeat based on the user input
                 //to count down (int i = 10; i > 0; i --)
             {
-                double result = Math.Pow(2, i);
+                double result = Math.Pow(2, i); // this is  a double because its a larger number than a whole 2 digit part. b.c we are raising the power of the number up to a 4 digit number. We could also console.convert into a double as well. 
+
+                // int result = Mat.Pow(2, i); 
+                // Console.Convert result double
                 Console.WriteLine(result);
             }
 
